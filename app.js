d7fef914612a572899a99c801590c2328e5d9755
Kyle <notxap55@yahoo.com>
2022-04-21 21:56:14 -0700

    need to restart

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 'use strict';
 
+function game() {
+
+    // for (let i = 0; i < 5; i++) {
+    //     playRound(playerSelection, computerSelection);
+    // }
+}
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log('draw');
-    } else console.log('not draw');
+        console.log(`Draw!`);
+        // winning conditions
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        // losing conditions
+    } else console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
 }
 
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     let randomNum = Math.floor(Math.random() * 3);
+
+    console.log(`comp choice ${options[randomNum]}`);
     return options[randomNum];
 }
 
 const playerSelection = 'rock';
 const computerSelection = computerPlay();
 
-console.log(playRound(playerSelection, computerSelection));
 
 // function playerPlay() {
 //     playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
